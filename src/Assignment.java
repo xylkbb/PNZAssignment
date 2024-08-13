60402d2427ff98d4e3e18092dff139634954817e
Eric Coe <coeeri001@warren.k12.in.us>
2024-08-13 13:29:17 -0400

    Initial commit

---
+++ src/Assignment.java
@@ -0,0 +1,25 @@
+public class Assignment
+{
+    public static void checkNumber(int number) {
+
+
+        if (number > 0){
+            System.out.println("Positive");
+        }
+        else if (number < 0){
+            System.out.println("Negative");
+        }
+        System.out.println("Zero");
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
